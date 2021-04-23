765cd275b63d58699d3f2de5d1c08b07935e963e
mrinal1224 <iammmrinalkc@gmail.com>
2021-04-23 20:27:31 +0530

    todo vanilla.js

---
+++ Browser DOM/ToDo/todo.js
@@ -0,0 +1,16 @@
+let input = document.querySelector(".input_box")
+let ul = document.querySelector(".task_list")
+
+input.addEventListener("keydown" ,function(e){
+    console.log("event object", e)
+    if(e.key=="Enter"){
+        let task = input.value;
+        console.log(task);
+        let li = document.createElement('li');
+        li.innerText=task;
+        ul.appendChild(li)
+        input.value = "";
+        
+       
+    }
+})
