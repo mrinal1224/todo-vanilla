c803af706b2e8b62c615be9c3213e4b634688ee0
mrinal1224 <iammrinalkc@gmail.com>
2021-04-24 11:12:15 +0530

    added styles and fonts

--- Browser DOM/ToDo/todo.js
+++ Browser DOM/ToDo/todo.js
@@ -1,20 +1,25 @@
-let input = document.querySelector(".input_box")
-let ul = document.querySelector(".task_list")
-
-input.addEventListener("keydown" ,function(e){
-    console.log("event object", e)
-    if(e.key=="Enter"){
-        let task = input.value;
-        console.log(task);
-        let li = document.createElement('li');
-        li.innerText=task;
-        ul.appendChild(li)
-
-        li.addEventListener("dblclick" , function(){
-            li.remove()
-        })
-        input.value = "";
+//  dom node 
+let input = document.querySelector(".input_box");
+let ul = document.querySelector(".task-list");
+// let li = document.querySelector(".task");
+input.addEventListener("keydown", function (e) {
+    //  e object -> describe -> event 
+    // console.log(" some key was pressed");
+    console.log("event object", e);
+    if (e.key == "Enter") {
+        // console.log("user want to enter a task");
         
-       
+        let task = input.value;
+        //  create any html tag 
+        // console.log(task);
+        let li = document.createElement("li");
+        li.innerText = task;
+        li.addEventListener("dblclick", function (e) {
+            li.remove();
+        })
+        //  add any attribute 
+        li.setAttribute("class", "task");
+        ul.appendChild(li);
+        input.value = "";
     }
-})
+});
